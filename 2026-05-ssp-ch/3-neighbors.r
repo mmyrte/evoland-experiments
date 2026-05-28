97b9647b059c4dd66253fd716a13224f4eb69d41
mmyrte <24587121+mmyrte@users.noreply.github.com>
2026-05-28 13:02:52 +0200

    neighbor generation

---
+++ 2026-05-ssp-ch/3-neighbors.r
@@ -0,0 +1,12 @@
+library(evoland)
+
+db <- evoland_db$new(path = "ssp-ch.evolanddb")
+
+# attn this may take a couple minutes
+db$set_neighbors(
+  max_distance = 1000,
+  distance_breaks = c(0, 100, 500, 1000)
+)
+
+# this however is pretty fast
+db$generate_neighbor_predictors()
